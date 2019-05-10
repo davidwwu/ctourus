703930a24e63e1b9086b88d51f006c6cbdffc93b
David Wu <david@wudavid.com>
2019-05-10 00:46:38 -0700

    try json parsing rsa private key for heroku

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ const sassMiddleware = require('node-sass-middleware');
 const axios = require('axios');
 const multer = require('multer');
 const jwt = require('jsonwebtoken');
-const privateKey = process.env.RSA_PRIVATE;
+const privateKey;
+if(process.env.NODE_ENV === 'production') {
+  privateKey = JSON.parse(process.env.RSA_PRIVATE);
+} else {
+  privateKey = process.env.RSA_PRIVATE;
+}
 
 
 // ----------------------------------------------------------------------------
