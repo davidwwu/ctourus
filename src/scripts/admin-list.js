9e2770650e50200bfc73ee509cfe5b9711b2c902
David Wu <david@wudavid.com>
2019-08-12 02:24:25 -0700

    Added quick edit modal for static pages - part 1

--- src/scripts/admin-list.js
+++ src/scripts/admin-list.js
@@ -42,12 +42,12 @@ $(function() {
     dialog.open();
   });
 
-  $('.copy-btn').on('click', function(e) {
-    let { _id, is_highlight, tour_id, tour_type, name, duration, starting_price, start_city, end_city,  } = $(this).data('tour');
+  $('.tour-copy-btn').on('click', function(e) {
+    let { _id, is_highlight, tour_id, tour_type, name, duration, starting_price, start_city, end_city } = $(this).data('tour');
     let menu = $(this).data('menu');
     
     // TODO - make sure to safe check id
-    $('#my-dialog-title').text('複製');
+    $('#my-dialog-title').text('複製線路');
     $('#modal-form').attr('action', `/admin/${tour_id}/duplicate`);
     $('#modal-is-highlight').prop('checked', is_highlight);
     $('#modal-tour-id').val(`${tour_id}-COPY`);
@@ -67,11 +67,11 @@ $(function() {
     dialog.open();
   });
 
-  $('.delete-btn').on('click', function(e) {
+  $('.tour-delete-btn').on('click', function(e) {
     let { _id, is_highlight, tour_id, tour_type, name, duration, starting_price, start_city, end_city } = $(this).data('tour');
     let menu = $(this).data('menu');
 
-    $('#my-dialog-title').text('刪除');
+    $('#my-dialog-title').text('刪除線路');
     $('#modal-form').attr('action', `/admin/${tour_id}/delete`);
     $('#modal-is-highlight').prop('checked', is_highlight);
     $('#modal-tour-id').val(tour_id);
@@ -87,6 +87,46 @@ $(function() {
     $('#modal-tour-type option').each(function(index) {
       if($(this).val() === menu.permalink) $(this).prop('selected', true);
     })
+    
+    dialog.open();
+  });
+
+  $('#new-static-page').on('click', function(e) {
+    $('#my-dialog-title').text('新靜態頁面');
+    $('#modal-form').attr('action', `/admin/create-static-page`);
+    $('#modal-order').val('');
+    $('#modal-title').val('');
+    $('#modal-permalink').val('');
+
+    $('#modal-order, #modal-title, #modal-permalink').prop('readonly', false);
+    
+    dialog.open();
+  });
+
+  $('.static-page-quick-edit').on('click', function(e) {
+    let { _id, order, name, permalink } = $(this).data('static-page');
+
+    $('#my-dialog-title').text('快速編輯');
+    $('#modal-form').attr('action', `/admin/static-pages/${permalink}/quick-edit`);
+    $('#modal-order').val(order);
+    $('#modal-title').val(name);
+    $('#modal-permalink').val(permalink);
+
+    $('#modal-order, #modal-title, #modal-permalink').prop('readonly', false);
+    
+    dialog.open();
+  });
+
+  $('.static-page-delete').on('click', function(e) {
+    let { _id, order, name, permalink } = $(this).data('static-page');
+
+    $('#my-dialog-title').text('刪除靜態頁面');
+    $('#modal-form').attr('action', `/admin/static-pages/${permalink}/delete`);
+    $('#modal-order').val(order);
+    $('#modal-title').val(name);
+    $('#modal-permalink').val(permalink);
+
+    $('#modal-order, #modal-title, #modal-permalink').prop('readonly', true);
     
     dialog.open();
   });
